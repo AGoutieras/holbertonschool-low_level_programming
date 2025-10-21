db0fce01470ad935d6fcfcb8458eed0ac5099bd1
AGoutieras <goutieras.anthony@gmail.com>
2025-10-21 09:36:25 +0000

    task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers of base16.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char base16;
+
+	for (base16 = '0'; base16 <= '9'; base16++)
+	{
+		putchar(base16);
+	}
+	for (base16 = 'a'; base16 <= 'f'; base16++)
+	{
+		putchar(base16);
+	}
+	putchar('\n');
+
+	return (0);
+}
