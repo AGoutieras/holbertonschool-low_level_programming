f4d583ff928d9f0ffcb2d77aede3160663cd9e07
AGoutieras <goutieras.anthony@gmail.com>
2025-10-21 09:17:29 +0000

    task 5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Prints the digits
- *		  from 0 to 9
+ * main - Prints the digits from 0 to 9
  *
  * Return: Always 0
  */
-
 int main(void)
 {
 	char digits;
 
 	for (digits = '0'; digits <= '9'; digits++)
-	{	
-	    putchar(digits);
+	{
+		putchar(digits);
 	}
 	putchar('\n');
 
