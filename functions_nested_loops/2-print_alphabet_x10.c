ae4698be679d07e3eb4a8e0e65e5fe5b29258b33
AGoutieras <goutieras.anthony@gmail.com>
2025-10-22 09:14:28 +0000

    task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints 10 times the alphabet,
+ *					in lowercase, followed by a new line.
+ */
+
+void print_alphabetx10(void)
+{
+	int count = 0;
+	char alphabet;
+
+	while (count++ <= 9)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
+			_putchar(alphabet);
+
+		_putchar('\n');
+	}
+}
