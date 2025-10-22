58b63b9b350f3fd4896b145e2015b63aad475a19
AGoutieras <goutieras.anthony@gmail.com>
2025-10-22 12:16:53 +0000

    task 6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -4,7 +4,7 @@
  * _abs - computes the absolute value of an integer.
  *
  * @n: the character to check.
- * Return: 1 if greater than 0, 0 if zero, -1 if less than zero
+ * Return: absolute value of the integer
  */
 
 int _abs(int n)
