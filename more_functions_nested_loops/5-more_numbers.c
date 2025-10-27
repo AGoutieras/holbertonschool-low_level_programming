c3791748697bb5c56fd4e4ef50894cb95ab435f6
AGoutieras <goutieras.anthony@gmail.com>
2025-10-27 10:06:24 +0000

    task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - 
+ *				
+ */
+
+void more_numbers(void)
+{
+	int count = 0, numbers;
+
+	while (count++ <= 10)
+	{
+		for (numbers = 0; numbers <= 14; numbers++)
+        {
+            if (numbers >= 10)
+                _putchar('1');
+            _putchar(numbers % 10 + '0');
+        }
+		_putchar('\n');
+	}
+}
