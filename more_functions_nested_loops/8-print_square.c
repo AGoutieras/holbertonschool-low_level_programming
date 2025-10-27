adb1d2db2ad2ae9165d7cfe8679276e1663a73a5
AGoutieras <goutieras.anthony@gmail.com>
2025-10-27 14:03:16 +0000

    task 8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -17,9 +17,11 @@ void print_square(int size)
 	{
 		for (height = 0; height < size; height++)
 		{
-			for (width = 0; width < height; width++)
+			for (width = 0; width < size; width++)
+			{
 				_putchar('#');
-
+			}
+			
 			_putchar('\n');
 		}
 	}
