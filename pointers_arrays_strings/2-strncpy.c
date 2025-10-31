48f34de2bdf31413a9b97abf2fa062c382576ae3
AGoutieras <goutieras.anthony@gmail.com>
2025-10-31 08:47:11 +0000

    task 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string.
+ * @dest: destination
+ * @src: source
+ * @n: bytes to copy
+ * Return: pointer to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		if (src[i] != '\0')
+			dest[i] = src[i];
+		else
+			dest[i] = '\0';
+	}
+
+	return (dest);
+}
