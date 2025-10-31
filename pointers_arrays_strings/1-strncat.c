95c2f9dc10ecc30ea874e04d3134cdf82afd3ed9
AGoutieras <goutieras.anthony@gmail.com>
2025-10-31 08:27:31 +0000

    task 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings.
+ * @dest: destination
+ * @src: source
+ * @n: bytes to copy
+ * Return: pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, pos = 0;
+
+	while (dest[index] != '\0')
+		index++;
+
+	while (src[pos] != '\0' && pos < n)
+	{
+		dest[index] = src[pos];
+		index++;
+		pos++;
+	}
+	dest[index] = '\0';
+
+	return (dest);
+}
