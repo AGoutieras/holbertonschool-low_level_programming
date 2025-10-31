547b4c8a7e22729c69ee58e1a63510a9cad0c671
AGoutieras <goutieras.anthony@gmail.com>
2025-10-31 12:47:33 +0000

    task *

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -10,24 +10,23 @@ char *leet(char *str)
 {
 	int index = 0;
 	char *letters = "aAeEoOtTlL";
-    char *numbers = "4433007711";
+	char *numbers = "4433007711";
 
-    while (str[index] != '\0')
-    {
-        int j = 0;
+	while (str[index] != '\0')
+	{
+		int j;
 
-        {
-        for (j = 0; letters[j]; j++)
-        {
-            if (str[index] == letters[j])
-            {
-                 str[index] = numbers[j];
-                break;
-            }
-        }
-        }
-        index++;
-    }
+		for (j = 0; letters[j]; j++)
+		{
+			if (str[index] == letters[j])
+			{
+				str[index] = numbers[j];
+				break;
+			}
+		}
 
-    return str;
+		index++;
+	}
+
+	return (str);
 }
