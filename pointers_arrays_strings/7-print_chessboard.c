ba67d96d1d1d1cdbe42fef8e794142f45e412f48
AGoutieras <goutieras.anthony@gmail.com>
2025-11-04 08:22:23 +0000

    Revert "task 7"
    This reverts commit a54f25847cb6c4ec1d54f55a01b0c582cb3f5bff.

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,20 +1,10 @@
 #include "main.h"
 
 /**
- * print_chessboard - prints the chessboard.
- * @a: chessboard
+ * print_chessboard - 
  */
 
 void print_chessboard(char (*a)[8])
 {
-	unsigned int w, h;
-
-	for (w = 0; w < 8; w++)
-	{
-		for (h = 0; h < 8; h++)
-		{
-			_putchar(a[w][h]);
-		}
-		_putchar('\n');
-	}
+    
 }
