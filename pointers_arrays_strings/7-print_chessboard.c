a54f25847cb6c4ec1d54f55a01b0c582cb3f5bff
AGoutieras <goutieras.anthony@gmail.com>
2025-11-04 08:21:40 +0000

    task 7

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,10 +1,20 @@
 #include "main.h"
 
 /**
- * print_chessboard - 
+ * print_chessboard - prints the chessboard.
+ * @a: chessboard
  */
 
 void print_chessboard(char (*a)[8])
 {
-    
+	unsigned int w, h;
+
+	for (w = 0; w < 8; w++)
+	{
+		for (h = 0; h < 8; h++)
+		{
+			_putchar(a[w][h]);
+		}
+		_putchar('\n');
+	}
 }
