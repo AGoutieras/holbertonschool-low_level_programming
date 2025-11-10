d189805746dabae59e123755b418509e70141ed5
AGoutieras <goutieras.anthony@gmail.com>
2025-11-10 09:07:42 +0000

    task 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main -  
+ */
+
+char *create_array(unsigned int size, char c)
+{
+    unsigned int index;
+    char *array;
+
+    if (size == 0)
+        return (NULL);
+
+    array = malloc(sizeof(char) * size);
+
+    if (array == NULL)
+        return (NULL);
+
+    for (index = 0; index < size; index++)
+        array[index] = c;
+
+    return (array);
+}
