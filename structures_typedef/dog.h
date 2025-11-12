399a7f9740796557bf0be08467032214bfad7713
AGoutieras <goutieras.anthony@gmail.com>
2025-11-12 12:35:41 +0000

    task 0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,18 @@
 #ifndef dog_h
 #define dog_h
 
-struct dog
+/**
+ * struct dog - structure.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ */
+
+typedef struct dog
 {
 	char name;
 	int age;
 	char owner;
-};
+} dog;
 
 #endif
