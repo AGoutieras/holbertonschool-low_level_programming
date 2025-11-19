b9500b6055e9d8f2835b280ecb16d52bde18124d
AGoutieras <goutieras.anthony@gmail.com>
2025-11-19 12:49:19 +0000

    .

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,16 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything.
+ * @format: 
+ */
+
+void print_all(const char * const format, ...);
+{
+    unsigned int index;
+
+    va_list args;
+
+    while (format[index] != '\0')
+    index++;
+}
