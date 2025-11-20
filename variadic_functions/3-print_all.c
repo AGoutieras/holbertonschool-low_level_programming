f675369f6da979cd45a65631c9dd351b44fb34a5
AGoutieras <goutieras.anthony@gmail.com>
2025-11-20 10:04:17 +0000

    task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,15 +2,46 @@
 
 /**
  * print_all - prints anything.
- * @format: 
+ * @format: type of argument
  */
 
-void print_all(const char * const format, ...);
+void print_all(const char * const format, ...)
 {
-    unsigned int index;
+	unsigned int index = 0, sep = 0;
+	char *str;
 
-    va_list args;
+	va_list args;
 
-    while (format[index] != '\0')
-    index++;
+	va_start(args, format);
+
+	while (format != NULL && format[index] != '\0')
+	{
+		if (sep)
+			printf(", ");
+		sep = 1;
+		switch (format[index])
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				break;
+			default:
+				sep = 0;
+				break;
+		}
+		index++;
+	}
+	printf("\n");
+	va_end(args);
 }
