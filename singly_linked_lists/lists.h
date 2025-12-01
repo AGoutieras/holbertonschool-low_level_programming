b5a4444b14abd4105fd504f4634ff5a94ffa55da
AGoutieras <goutieras.anthony@gmail.com>
2025-12-01 11:04:32 +0000

    Added prototype of add_node function

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,6 +2,8 @@
 #define lists_h
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -13,12 +15,13 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif
