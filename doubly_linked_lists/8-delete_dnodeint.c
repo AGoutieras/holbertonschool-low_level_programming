501e8e85df05833fd5364b91237376592fce2225
AGoutieras <goutieras.anthony@gmail.com>
2025-12-03 14:23:05 +0000

    Document head parameter in delete_dnodeint_at_index

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -3,7 +3,7 @@
 /**
  * delete_dnodeint_at_index - deletes the node at index
  *							  of a dlistint_t linked list.
- * @head:
+ * @head: head of the list
  * @index: index of the node that should be deleted.
  *
  * Return: 1 if it succeeded, -1 if it failed
