9fd0225e8d54a3b6719039426b539e7eef66633d
AGoutieras <goutieras.anthony@gmail.com>
2025-12-03 11:57:27 +0000

    Fix variable name typo in insert_dnodeint_at_index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i;
-	dlistint_t *current, *new_node
+	dlistint_t *current, *new_node;
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
@@ -31,15 +31,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint_end(h, n));
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+	if (new_node == NULL)
 		return (NULL);
 
-		new_node->n = n;
-		new_node->prev = current;
-		new_node->next = current->next;
+	new_node->n = n;
+	new_node->prev = current;
+	new_node->next = current->next;
 
-		current->next->prev = new_node;
-		current->next = new_node;
+	current->next->prev = new_node;
+	current->next = new_node;
 
-		return (new_node);
+	return (new_node);
 }
