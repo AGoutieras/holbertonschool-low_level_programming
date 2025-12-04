880e86fde5925bbd6b6c4343c77eec36751febb3
AGoutieras <goutieras.anthony@gmail.com>
2025-12-04 11:47:08 +0000

    Improve error handling in read_textfile function

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -25,22 +25,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	fd = open(filename, O_RDONLY);
 
-	if (fd == -1)
-		return (0);
-
 	r = read(fd, buffer, letters);
-
-	if (r == -1)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
 
 	w = write(1, buffer, r);
 
-	if (w != r)
+	if (fd == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer)
 		return (0);
+	}
 
 	free(buffer);
 	close(fd);
