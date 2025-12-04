5c63a50398f69d262cfdc9ffe1be412a9082b799
AGoutieras <goutieras.anthony@gmail.com>
2025-12-04 11:38:57 +0000

    Revert "Fix 1 added cleanup before return of w != r"
    This reverts commit d31e5b161d3972907c4404144856eda849f79cb6.

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -32,6 +32,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	r = read(fd, buffer, letters);
+
 	if (r == -1)
 	{
 		free(buffer);
@@ -40,12 +41,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	w = write(1, buffer, r);
+
 	if (w != r)
-	{
-		free(buffer)
-		close(fd)
 		return (0);
-	}
 
 	free(buffer);
 	close(fd);
