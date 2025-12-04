bdf50deb5904496a867a052f076c2e346c374800
AGoutieras <goutieras.anthony@gmail.com>
2025-12-04 11:44:13 +0000

    Revert "Fix 1 added cleanup before return of w != r"
    This reverts commit d31e5b161d3972907c4404144856eda849f79cb6.

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -19,17 +19,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
+	buffer = malloc(letters + 1);
+	if (buffer == NULL)
+		return (0);
+
 	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 		return (0);
-
-	buffer = malloc(letters + 1);
-	if (buffer == NULL)
-	{
-		close(fd);
-		return (0);
-	}
 
 	r = read(fd, buffer, letters);
 
